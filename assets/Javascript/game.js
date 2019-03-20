5beaf12c387b84c9d7bc3489eda3f975eafeae93
CodeRed <antonio.martinez1653204@gmail.com>
2019-03-20 18:31:14 -0500

    starting off with JS

---
+++ assets/Javascript/game.js
@@ -0,0 +1,21 @@
+//Theme Smashbros
+//Set up variables for smash bro characters
+    //set up seperate variables for letters?
+    // make sure to find a way to use _ _ _ _
+//set of onkeyup
+//Should I use conditionals or for loop?
+    //
+//Bootstrap or Personal Css
+//HTML set up 
+
+var SmashBrosChar = ['Mario', 'Peach','Luigi','Cloud',];
+
+document.onkeyup(event) {
+
+    var userguess= event.key;
+
+    var CPUlv9= SmashBrosChar [Math.floor(Math.random()*SmashBrosChar.length]
+}
+
+
+
