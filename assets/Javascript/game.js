ab085450ff306ff4cd19a592f6582186697f227e
CodeRed <antonio.martinez1653204@gmail.com>
2019-03-23 23:30:27 -0500

    Not finished but this is all I have for now

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,22 +1,19 @@
-//Theme Smashbros
-//Set up variables for smash bro characters
-    //set up seperate variables for letters?
-    // make sure to find a way to use _ _ _ _
-//set Math.floor(Math.random) to create random word. 
-//Need to track guesses
-//Put guess letters in a seperate array 
-// Create a losing loop
-    //
-//Bootstrap or Personal Css
-//HTML set up 
-//
 
+//Variables
 var SmashBrosChar = ['Mario', 'Peach','Luigi','Cloud','Mewtwo', 'Ganondorf', 'Link'];
 var maxGuesses= 9;
+var remainingGuesses= 0
 var guessedletters= [];
+var win= 0;
+var losses = 0;
 
 // Random word
 var SmashBrosChar= SmashBrosChar[Math.floor(Math.random()*SmashBrosChar.length)];
+
+
+//function to reset game 
+
+
 
 //Answer Array where I create the _ _ _ _ for the word guess
 var hiddenword = [];
@@ -36,17 +33,17 @@ while (remainingletters > 0 ) {
     
     // This is my game starter. 
     var ThePlayer = prompt ("Guess a letter, or click Cancel to stop playing.");
-        if (ThePlayer === null) {//This is if the player hits cancel they will end the loop and exit the game 
-        break;
+    if (ThePlayer === null) {//This is if the player hits cancel they will end the loop and exit the game 
+    break;
 
 } else if (ThePlayer.length != 1) {
-    alert("Please enter a single letter.");
+alert("Please enter a single letter.");
 
 } else {
     //every time the player guesses the game needs to update 
     for (var j = 0; j < SmashBrosChar.length; j++) {
         if (SmashBrosChar[j] === ThePlayer) {
-            hiddenword[j] = ThePlayer.toLowerCase(); 
+            hiddenword[j] = ThePlayer; 
             remainingletters--; 
         }
     }
@@ -55,3 +52,8 @@ while (remainingletters > 0 ) {
     //Showing the answer 
 alert(hiddenword.join(" "));
 alert ("Time! The winner is " + SmashBrosChar);
+
+
+  
+
+   
